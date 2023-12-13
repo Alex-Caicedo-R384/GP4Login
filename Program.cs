37a2bf416f4c714d3e0bcf7c54fa92234e5ac424
Alex_Caicedo_R384 <alex.caicedo@udla.edu.ec>
2023-12-12 23:11:52 -0500

    .

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ using Microsoft.Extensions.DependencyInjection;
 using System.Net.Http;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<Login>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
